94818a6fb8f737b194c6479d88408744bce0b374
David Alber <alber.david@gmail.com>
2018-10-13 23:58:14 -0700

    Add hex to base 64 transcoder

---
+++ 1/one/src/lib.rs
@@ -0,0 +1,80 @@
+fn hex_to_byte(hex: char) -> u8 {
+    match hex {
+        '0' => 0,
+        '1' => 1,
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        'a' => 10,
+        'b' => 11,
+        'c' => 12,
+        'd' => 13,
+        'e' => 14,
+        'f' => 15,
+        _ => panic!("{} is not a valid hex value", hex)
+    }
+}
+
+fn hex_to_bytes(hex: &str) -> Vec<u8> {
+    let mut bytes = vec![0; hex.len()/2];
+    let hex = hex.chars().enumerate();
+    for (i, c) in hex {
+        let byte_ind = i/2;
+        match i % 2 {
+            0 => { bytes[byte_ind] = hex_to_byte(c) },
+            1 => { bytes[byte_ind] = (bytes[byte_ind] << 4) | hex_to_byte(c) },
+            _ => ()
+        };
+    }
+
+    bytes
+}
+
+fn int_to_base64(val: u32) -> char {
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    return alphabet.chars().nth(val as usize).unwrap()
+}
+
+fn bytes_to_base64(bytes: Vec<u8>) -> String {
+    let mut segments: Vec<u32> = vec![0; ((bytes.len() as f32)/3.0).ceil() as usize];
+
+    for (i, byte) in bytes.iter().rev().enumerate() {
+        let byte: u32 = *byte as u32;
+        segments[i/3] |= byte << 8*(i % 3);
+    }
+
+    let mut base64: Vec<char> = Vec::with_capacity(segments.len()*4);
+    for segment in segments {
+        for shift in 0..4 {
+            base64.push(int_to_base64((segment >> 6*shift) & 63));            
+        }
+    }
+
+    base64.into_iter().rev().collect()
+}
+
+pub fn hex_to_base64(hex: &str) -> String {
+    bytes_to_base64(hex_to_bytes(hex))
+}
+
+#[cfg(test)]
+mod tests {
+    use *;
+
+    #[test]
+    fn test_bytes_to_base64() {
+        assert_eq!(bytes_to_base64(vec![2, 3, 120]),
+         "AgN4");
+    }
+
+    #[test]
+    fn test_hex_to_base64() {
+        assert_eq!(hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
+         "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+    }
+}
---
+++ 1/one/src/main.rs
@@ -0,0 +1,8 @@
+extern crate one;
+
+use one::hex_to_base64;
+
+fn main() {
+    let hex = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    println!("{} -> {}", hex, hex_to_base64(&hex));
+}
