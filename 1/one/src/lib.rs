911ab2cd7887b15615754b0a7319a066cdcbb60d
David Alber <alber.david@gmail.com>
2018-10-14 00:12:03 -0700

    Format code

--- 1/one/src/lib.rs
+++ 1/one/src/lib.rs
@@ -16,19 +16,19 @@ fn hex_to_byte(hex: char) -> u8 {
         'd' => 13,
         'e' => 14,
         'f' => 15,
-        _ => panic!("{} is not a valid hex value", hex)
+        _ => panic!("{} is not a valid hex value", hex),
     }
 }
 
 fn hex_to_bytes(hex: &str) -> Vec<u8> {
-    let mut bytes = vec![0; hex.len()/2];
+    let mut bytes = vec![0; hex.len() / 2];
     let hex = hex.chars().enumerate();
     for (i, c) in hex {
-        let byte_ind = i/2;
+        let byte_ind = i / 2;
         match i % 2 {
-            0 => { bytes[byte_ind] = hex_to_byte(c) },
-            1 => { bytes[byte_ind] = (bytes[byte_ind] << 4) | hex_to_byte(c) },
-            _ => ()
+            0 => bytes[byte_ind] = hex_to_byte(c),
+            1 => bytes[byte_ind] = (bytes[byte_ind] << 4) | hex_to_byte(c),
+            _ => (),
         };
     }
 
@@ -37,21 +37,21 @@ fn hex_to_bytes(hex: &str) -> Vec<u8> {
 
 fn int_to_base64(val: u32) -> char {
     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    return alphabet.chars().nth(val as usize).unwrap()
+    return alphabet.chars().nth(val as usize).unwrap();
 }
 
 fn bytes_to_base64(bytes: Vec<u8>) -> String {
-    let mut segments: Vec<u32> = vec![0; ((bytes.len() as f32)/3.0).ceil() as usize];
+    let mut segments: Vec<u32> = vec![0; ((bytes.len() as f32) / 3.0).ceil() as usize];
 
     for (i, byte) in bytes.iter().rev().enumerate() {
         let byte: u32 = *byte as u32;
-        segments[i/3] |= byte << 8*(i % 3);
+        segments[i / 3] |= byte << 8 * (i % 3);
     }
 
-    let mut base64: Vec<char> = Vec::with_capacity(segments.len()*4);
+    let mut base64: Vec<char> = Vec::with_capacity(segments.len() * 4);
     for segment in segments {
         for shift in 0..4 {
-            base64.push(int_to_base64((segment >> 6*shift) & 63));            
+            base64.push(int_to_base64((segment >> 6 * shift) & 63));
         }
     }
 
@@ -76,21 +76,29 @@ mod tests {
 
     #[test]
     fn test_hex_to_bytes() {
-        assert_eq!(hex_to_bytes("000102030405060708090a0b0c0d0e0ff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"), vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255]);
+        assert_eq!(
+            hex_to_bytes("000102030405060708090a0b0c0d0e0ff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"),
+            vec![
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 240, 241, 242, 243, 244, 245,
+                246, 247, 248, 249, 250, 251, 252, 253, 254, 255
+            ]
+        );
     }
 
     #[test]
     fn test_int_to_base64() {
         let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
         for val in 0..64 {
-            assert_eq!(int_to_base64(val), alphabet.chars().nth(val as usize).unwrap());
+            assert_eq!(
+                int_to_base64(val),
+                alphabet.chars().nth(val as usize).unwrap()
+            );
         }
     }
 
     #[test]
     fn test_bytes_to_base64() {
-        assert_eq!(bytes_to_base64(vec![2, 3, 120]),
-         "AgN4");
+        assert_eq!(bytes_to_base64(vec![2, 3, 120]), "AgN4");
     }
 
     #[test]
