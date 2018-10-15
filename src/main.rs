dbe3ddf8186259a5dc78e95bb3d7391a1c88a8f7
David Alber <alber.david@gmail.com>
2018-10-15 08:46:33 -0700

    Add links to challenges

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,20 @@ mod set1 {
                                     's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     pub fn challenge1() {
+        // https://cryptopals.com/sets/1/challenges/1
         let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         println!("{} -> {}", hex, hex_to_base64(hex));
     }
 
     pub fn challenge2() {
+        // https://cryptopals.com/sets/1/challenges/2
         let hex1 = "1c0111001f010100061a024b53535009181c";
         let hex2 = "686974207468652062756c6c277320657965";
         println!("{} ^ {} -> {}", hex1, hex2, hex_xor(hex1, hex2));
     }
 
     pub fn challenge3() {
+        // https://cryptopals.com/sets/1/challenges/3
         let encrypted = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 
         let winner = ASCII_LOWER.iter().map(|l| {
